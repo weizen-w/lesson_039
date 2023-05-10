b69939ae09badf40fb00a1cd4e4b112bc446bcf6
Wladimir <weizen.w@gmx.de>
2023-05-10 23:05:28 +0200

    feat: add abstract class House

--- src/Houses/House.java
+++ src/Houses/House.java
@@ -1,7 +1,22 @@
 package Houses;
 
-public class House {
+public abstract class House {
 //  Создайте абстрактный класс "Дом". В качестве аргумента конструктора принимайте адрес.
 //  Напишите для адреса сеттер и геттер.
 
+  private String address;
+
+  public House(String address) {
+    this.address = address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public abstract String toString();
 }
