cea85c9cb9849b15f5bbaac9ac5288d2a94990f8
Wladimir <weizen.w@gmx.de>
2023-05-10 23:06:20 +0200

    feat: add classes ApartmentHouse and PrivateHouse

--- src/Houses/ApartmentHouse.java
+++ src/Houses/ApartmentHouse.java
@@ -1,9 +1,24 @@
 package Houses;
 
-public class ApartmentHouse {
+public class ApartmentHouse extends House {
 //  Создайте классы "Частный дом" и "Многоквартирный дом" - наследники класса "Дом".
 //  При создании многоквартирного дома дополнительно принимайте в конструкторе
 //  целое число - количество подъездов.
 //  В методе toString() выводите в удобном виде всю известную информацию о доме.
 
+  private final int porch;
+
+  public ApartmentHouse(String address, int porch) {
+    super(address);
+    this.porch = porch;
+  }
+
+  public int getPorch() {
+    return porch;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Многоквартирный дом (%d подъездов) по адресу: %s", porch, getAddress());
+  }
 }
--- src/Houses/PrivateHouse.java
+++ src/Houses/PrivateHouse.java
@@ -1,9 +1,17 @@
 package Houses;
 
-public class PrivateHouse {
+public class PrivateHouse extends House {
 //  Создайте классы "Частный дом" и "Многоквартирный дом" - наследники класса "Дом".
 //  При создании многоквартирного дома дополнительно принимайте в конструкторе
 //  целое число - количество подъездов.
 //  В методе toString() выводите в удобном виде всю известную информацию о доме.
 
+  public PrivateHouse(String address) {
+    super(address);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Частный дом по адресу: %s", getAddress());
+  }
 }
